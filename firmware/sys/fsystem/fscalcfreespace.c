d2ff74d7f1968435a10cfc01a273fb40f28f83ca
claudio <cl@noemail.no>
2016-08-08 23:07:22 -0400

    Added new limited scan strategy

--- firmware/sys/fsystem/fscalcfreespace.c
+++ firmware/sys/fsystem/fscalcfreespace.c
@@ -5,7 +5,6 @@
 * See the file LICENSE.txt that shipped with this distribution.
 */
 
-
 #include "fsyspriv.h"
 
 
@@ -193,6 +192,208 @@ if(freestart && (fs->FreeAreaSize<freesize)) { fs->NextFreeCluster=freestart; fs
 simpfree(buffer);
 fs->FreeAreaSize<<=fs->ClusterSize-9;
 fs->FreeSpace=freecount<<(fs->ClusterSize-9);
+fs->InitFlags|=2;
+return FS_OK;
+
+}
+
+
+
+// LIMITED SCAN ON THE FAT (3 sectors), LOOKING FOR FREE SPACE
+
+int FSScanFreeSpace(FS_VOLUME *fs,unsigned int nextfreecluster)
+{
+unsigned char *buffer;
+unsigned int fataddr,startfataddr;
+int value;
+unsigned int maxaddr=0;
+unsigned int freecount,freestart,freesize;
+int limit,f;
+
+buffer=simpmallocb(1536);
+if(!buffer) return FS_ERROR;
+
+switch(fs->FATType)
+{
+case 1:
+maxaddr=(( ((fs->TotalClusters)&(~1))+2)*3)/2;
+fataddr=(( ((nextfreecluster)&(~1)))*3)/2;
+if(fataddr&1) fataddr--;
+if(fs->TotalClusters&1) maxaddr--;
+break;
+case 2:
+maxaddr=(fs->TotalClusters+2)<<1;
+fataddr=nextfreecluster<<1;
+break;
+case 3:
+maxaddr=(fs->TotalClusters+2)<<2;
+fataddr=nextfreecluster<<2;
+break;
+}
+
+fataddr/=1536;
+fataddr*=1536;      // ALIGN TO 3 SECTOR GROUP
+
+freecount=0;
+freestart=0;
+freesize=0;
+fs->FreeAreaSize=0;
+
+startfataddr=fataddr;
+
+do {
+
+if(SDDRead((((uint64_t)fs->FirstFATAddr)<<9)+fataddr,1536,buffer,fs->Disk)!=1536) {
+simpfree(buffer);
+return FS_ERROR;
+}
+
+FSPatchFATBlock(buffer,1536,fataddr,fs,FALSE);		// GET UPDATED FAT INFORMATION
+
+// SCAN FAT BLOCK
+if(maxaddr-fataddr<1536) limit=maxaddr-fataddr;
+else limit=1536;
+
+
+switch(fs->FATType)
+{
+case 1:
+// FAT 12 SCANNING
+    for(f=0;f<limit;f+=3)
+    {
+        value=buffer[f]+(buffer[f+1]<<8)+(buffer[f+2]<<16);
+        if(!value) {
+            // PROCESS 2 CLUSTERS AT A TIME
+            freecount+=2;
+            if(!freestart) {
+                freestart=FSFATEntry2Addr(fataddr+f,fs);
+                freesize=2;
+            }
+            else freesize+=2;
+        }
+        else {
+            // PROCESS EACH CLUSTER INDIVIDUALLY
+            if(value&0xfff) {
+                if(freestart && (fs->FreeAreaSize<freesize)) { fs->NextFreeCluster=freestart; fs->FreeAreaSize=freesize; }
+                freestart=0;
+            }
+            else {
+                ++freecount;
+                if(!freestart) {
+                    freestart=FSFATEntry2Addr(fataddr+f,fs);
+                    freesize=1;
+                }
+                else freesize++;
+            }
+
+            if(value>>12) {
+                if(freestart && (fs->FreeAreaSize<freesize)) { fs->NextFreeCluster=freestart; fs->FreeAreaSize=freesize; }
+                freestart=0;
+            }
+            else {
+                ++freecount;
+                if(!freestart) {
+                    freestart=FSFATEntry2Addr(fataddr+f+1,fs);
+                    freesize=1;
+                }
+                else freesize++;
+            }
+        }
+
+    }
+
+    if(f!=limit) {
+        // FAT HAS ODD-NUMBER OF CLUSTERS
+
+        value=buffer[f]+((buffer[f+1]&0xf)<<8);
+            // PROCESS EACH CLUSTER INDIVIDUALLY
+            if(value&0xfff) {
+                if(freestart && (fs->FreeAreaSize<freesize)) { fs->NextFreeCluster=freestart; fs->FreeAreaSize=freesize; }
+                freestart=0;
+            }
+            else {
+                ++freecount;
+                if(!freestart) {
+                    freestart=FSFATEntry2Addr(fataddr+f,fs);
+                    freesize=1;
+                }
+                else freesize++;
+            }
+
+
+
+
+
+
+    }
+
+
+break;
+
+case 2:
+// FAT 16 SCANNING
+
+for(f=0;f<limit;f+=2)
+{
+value=*(unsigned short int *)(buffer+f);
+if(!value) {
+// PROCESS 2 CLUSTERS AT A TIME
+freecount++;
+if(!freestart) {
+freestart=FSFATEntry2Addr(fataddr+f,fs);
+freesize=1;
+}
+else freesize++;
+}
+else {
+if(freestart && (fs->FreeAreaSize<freesize)) { fs->NextFreeCluster=freestart; fs->FreeAreaSize=freesize; }
+freestart=0;
+}
+}
+break;
+
+case 3:
+
+// FAT 32 SCANNING
+
+for(f=0;f<limit;f+=4)
+{
+value=*(unsigned int *)(buffer+f);
+if(!value) {
+// PROCESS 2 CLUSTERS AT A TIME
+freecount++;
+if(!freestart) {
+freestart=FSFATEntry2Addr(fataddr+f,fs);
+freesize=1;
+}
+else freesize++;
+}
+else {
+if(freestart && (fs->FreeAreaSize<freesize)) { fs->NextFreeCluster=freestart; fs->FreeAreaSize=freesize; }
+freestart=0;
+}
+
+}
+break;
+
+}
+
+fataddr+=limit;
+
+if(fataddr>=maxaddr) fataddr=0;     // WRAP AROUND AND START OVER
+
+} while( (fataddr!=startfataddr) && (freecount==0));
+
+if(fataddr==startfataddr) {
+    fs->FreeAreaSize=0;
+    simpfree(buffer);
+    return FS_DISKFULL;
+}
+
+if(freestart && (fs->FreeAreaSize<freesize)) { fs->NextFreeCluster=freestart; fs->FreeAreaSize=freesize; }
+
+simpfree(buffer);
+fs->FreeAreaSize<<=fs->ClusterSize-9;
 return FS_OK;
 
 }
