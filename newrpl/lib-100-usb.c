032ac3f580cb86644b143800ef8efcfc9a83593a
claudiol <claudiol@sourceforge.net>
2019-05-28 18:43:55 -0400

    Fixed USBAUTORCV (missing backport).

--- newrpl/lib-100-usb.c
+++ newrpl/lib-100-usb.c
@@ -253,27 +253,22 @@ void LIB_HANDLER()
             return;
         }
 
-        // GET MINIMAL STORAGE FIRST, 64 bytes=8 words
-        newobj=rplAllocTempOb(8);
+        // GET MINIMAL STORAGE FIRST, 64 bytes=16 words
+        newobj=rplAllocTempOb(16);
         if(!newobj) {
             // INSUFFICIENT MEMORY
             return;
         }
-        allocated=8;
+        allocated=16;
 
         switch(usb_filetype(fileid))
         {
         case 'O':   // THIS IS AN RPL OBJECT
             newobjptr=newobj;
-            bytesread=usb_fileread(fileid,(BYTEPTR)newobjptr,4);  // GET THE OBJECT PROLOG
-            if(bytesread<4) { rplError(ERR_USBCOMMERROR); break; }
-            expectedsize=rplObjSize(newobj);
-            newobjptr+=4;
             break;
         case 'D':   // THIS IS ARBITRARY BINARY DATA
             newobjptr=newobj+4;
             newobj[0]=MKPROLOG(DOBINDATA,0);    // WE DON'T KNOW THE SIZE YET
-            expectedsize=0;
             break;
         case 'B':   // THIS IS A BACKUP
         case 'W':   // THIS IS A FIRMWARE UPDATE
@@ -284,27 +279,30 @@ void LIB_HANDLER()
             break;
         }
 
-        do {
+        while(!Exceptions) {
             offset=newobjptr-newobj;
             bytesread=usb_fileread(fileid,(BYTEPTR)newobjptr,(allocated-offset)*sizeof(WORD));
             newobjptr+=(bytesread+3)>>2;
             if(bytesread<(allocated-offset)*sizeof(WORD)) {
-                if(!usb_eof(fileid)) rplError(ERR_USBTIMEOUT);
-                break;
+
+                if(bytesread==0) {
+                    if(!usb_eof(fileid)) rplError(ERR_USBTIMEOUT);
+                    break;
+                }
             }
             // MORE DATA IS EXPECTED, ALLOCATE MORE MEMORY
 
             ScratchPointer1=newobj;
             offset=newobjptr-newobj;
-            needwords=(expectedsize? (expectedsize-allocated+1):8);
-            if(needwords>0) rplResizeLastObject(needwords);
-            allocated+=needwords;
+            rplResizeLastObject(16);
+            allocated+=16;
             newobj=ScratchPointer1;
             newobjptr=newobj+offset;
-        } while(!Exceptions);
+        }
 
         //  WE ARE DONE WITH THE TRANSMISSION
         usb_rxfileclose(fileid);
+
 
         if(Exceptions) return;
 
@@ -312,7 +310,11 @@ void LIB_HANDLER()
         switch(usb_filetype(fileid))
         {
         case 'O':   // THIS IS AN RPL OBJECT
-            if(!rplVerifyObject(newobj)) {
+            if(offset<rplObjSize(newobj)) {     // CHECK IF WE RECEIVED A COMPLETE OBJECT
+                rplError(ERR_USBINVALIDDATA);
+                return;
+            }
+            if(!rplVerifyObject(newobj)) {      // AND ALSO THAT THE OBJECT IS VALID
                 rplError(ERR_USBINVALIDDATA);
                 return;
             }
@@ -326,8 +328,6 @@ void LIB_HANDLER()
 
         // WE HAVE A VALID OBJECT!
 
-
-        // WE HAVE A VALID OBJECT!
         rplOverwriteData(1,newobj);
         rplPushTrue();
         return;
