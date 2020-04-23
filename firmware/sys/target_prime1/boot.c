ca3d05fab2a91c9e442c44202c9ab27167023e97
Stefan Achatz <erazor_de@users.sourceforge.net>
2020-04-23 21:36:50 +0200

    Header of Prime G1 firmware image is added at compile time
    Replacing external header data with preamble data.
    External file size correction is still needed. Maybe this could be done
    in linker script?

--- firmware/sys/target_prime1/boot.c
+++ firmware/sys/target_prime1/boot.c
@@ -7,15 +7,22 @@
 
 #include <newrpl.h>
 
-__ARM_MODE__ void startup(int) __attribute__((naked, noreturn));
-void startup(int prevstate)
+static void setup_hardware()
 {
+	// Set 3 LED pins as outputs
     uint32_t config = *GPCCON;
     config &= 0xFFFF03FF;
     config |= 0x00005400;
     *GPCCON = config;
+}
+
+__ARM_MODE__ void startup(int) __attribute__((naked, noreturn));
+void startup(int prevstate)
+{
+	setup_hardware();
 
     uint32_t pins = *GPCDAT;
+    // Pin GPC7 is red LED
     pins |= 0x000000E0;
     *GPCDAT = pins;
 
