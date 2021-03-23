66296cc1123b7da7466e3a6a4eb485138943cabe
claudiol <claudiol@users.sourceforge.net>
2021-03-23 17:28:49 -0400

    Added missing include to build old newRPL Desktop.

--- firmware/include/xgl.h
+++ firmware/include/xgl.h
@@ -3,7 +3,9 @@
 
 // Select the proper library according to the target (4-bit grayscale or full color)
 
-
+#ifdef TARGET_PC
+#include "ggl.h"
+#endif
 #ifdef TARGET_PC_PRIMEG1
 #include "cgl.h"
 #endif
