6e070f69955e3a13c8a5e0e90ddc8661196b1f49
Christophe de Dinechin <christophe@dinechin.org>
2022-09-18 02:00:11 +0100

    Initialize missing fields in ggl_initsrc
    Signed-off-by: Christophe de Dinechin <christophe@dinechin.org>

--- firmware/ggl/ggl_initscr.c
+++ firmware/ggl/ggl_initscr.c
@@ -11,17 +11,20 @@ void ggl_initscr(gglsurface *srf)
 {
     srf->pixels = (pixword *) MEM_PHYS_SCREEN;
     srf->width  = LCD_SCANLINE;
-    srf->x = srf->y = 0;
-    srf->left = srf->top = 0;
-    srf->right             = LCD_W - 1;
-    srf->bottom             = LCD_H - 1;
-#ifdef TARGET_PRIME1
-    srf->active_buffer = 0;
-#  if SCREEN_BUFFERS > 1
+    srf->height = LCD_H;
+    srf->x      = 0;
+    srf->y      = 0;
+    srf->left   = 0;
+    srf->top    = 0;
+    srf->right  = LCD_W - 1;
+    srf->bottom = LCD_H - 1;
+
+#if SCREEN_BUFFERS > 1
     srf->active_buffer = lcd_getactivebuffer();
     srf->pixels += LCD_W * LCD_H / PIXELS_PER_WORD * srf->active_buffer;
-#  endif
-#endif /* TARGET_PRIME1 */
+#else
+    srf->active_buffer = 0;
+#endif
 }
 
 // Global palette, can be used for grayscale conversion or for themes
